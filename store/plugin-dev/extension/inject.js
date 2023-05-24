9be984f9c1bd91881138de0727bf1d9663f81a14
Milan Hirpara <milan.encodedots@gmail.com>
2023-05-24 18:38:16 +0530

    URL change

--- store/plugin-dev/extension/inject.js
+++ store/plugin-dev/extension/inject.js
@@ -1,5 +1,5 @@
 (function(window, undefined) {
-	var URL_TO_PLUGIN = "https://milan-encodedots.github.io/onlyoffice.github.io/sdkjs-plugins/content/propact/";
+	var URL_TO_PLUGIN = "https://github.com/milan-encodedots/onlyoffice.github.io/sdkjs-plugins/content/propact/";
 	
 	var xhrObj = new XMLHttpRequest();
 	xhrObj.open('GET', URL_TO_PLUGIN + "config.json", false);
